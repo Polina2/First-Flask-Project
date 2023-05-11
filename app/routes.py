74fac8d8e2310b56b042f71b5f7906ebf44c5b0b
Haku <polina231003@mail.ru>
2023-05-12 00:04:26 +0300

    encoding and queries

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, request
 from app import app
+from app.models import PhoneNumbers, MovedNumbers
 
 
 @app.route('/')
@@ -12,4 +13,16 @@ def index():
 
     region = ''
     mobile_operator = ''
+
+    if 'tel' in params_post:
+        request_number = params_post['tel']
+        request_code = request_number[:3]
+        number_without_code = request_number[3:]
+        pn = PhoneNumbers.query.filter(PhoneNumbers.code == request_code)\
+            .filter(PhoneNumbers.begin <= number_without_code)\
+            .filter(PhoneNumbers.end >= number_without_code).first()
+        mn = MovedNumbers.query.get(request_number)
+        if pn is not None:
+            region = pn.region
+            mobile_operator = pn.mobile_operator if mn is None else mn.mobile_operator
     return render_template('index.html', region=region, mobile_operator=mobile_operator)
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:' + open("secrets.txt", 'r').read() + '@localhost:5432/tel_app'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:' + open("secrets.txt", 'r').read() + '@localhost:5432/tel_app1'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
