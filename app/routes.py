737d8b4d661d71d0d5798b8831dc49784514cc74
Haku <polina231003@mail.ru>
2023-05-11 12:38:55 +0300

    submit form

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,10 @@ from app import app
 @app.route('/')
 @app.route('/index', methods=['POST'])
 def index():
-    return render_template('index.html')
+    params_post = {}
+    for p in request.form:
+        params_post[p] = request.form[p]
+
+    region = ''
+    mobile_operator = ''
+    return render_template('index.html', region=region, mobile_operator=mobile_operator)
