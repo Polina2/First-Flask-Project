acfe38ad0d9462ace9b183ecc54399dc505386df
Haku <polina231003@mail.ru>
2023-05-11 12:17:17 +0300

    background and form

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['POST'])
 def index():
     return render_template('index.html')
